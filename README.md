9bc3512ac41e3138ade922d2b126e7bda980fb2b
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-02-05 16:33:29 -0500

    2020 refresh/make public

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-# electron-quick-start
+# Cover Letter Generator
 
-**Clone and run for a quick way to see Electron in action.**
-
-This is a minimal Electron application based on the [Quick Start Guide](http://electron.atom.io/docs/tutorial/quick-start) within the Electron documentation.
-
-**Use this app along with the [Electron API Demos](http://electron.atom.io/#get-started) app for API code examples to help you get started.**
-
-A basic Electron application needs just these files:
-
-- `package.json` - Points to the app's main file and lists its details and dependencies.
-- `main.js` - Starts the app and creates a browser window to render HTML. This is the app's **main process**.
-- `index.html` - A web page to render. This is the app's **renderer process**.
-
-You can learn more about each of these components within the [Quick Start Guide](http://electron.atom.io/docs/tutorial/quick-start).
+This is a cover letter generator that I only used a handful of times. While this is not a tool that I'd recommend (its best to tailer a cover letter to a job if you're going to do it at all) i'm leaving it here as a learning tool  to refer to later in learning the electon platform.
 
 ## To Use
 
@@ -29,7 +17,17 @@ npm install
 npm start
 ```
 
+### Customizing Application
+The index.html file needs to be edited for how you want your letter to read.
+Then you need to enter the information specific to the job (Company name, job position, skills pertinent to job)
+Please do not enter any punctuation.
+
+### Copying Text
+After generating the message, you can press the copy text button to copy the message to post it to online job postings.
+
 Note: If you're using Linux Bash for Windows, [see this guide](https://www.howtogeek.com/261575/how-to-run-graphical-linux-desktop-applications-from-windows-10s-bash-shell/) or use `node` from the command prompt.
+
+
 
 ## Resources for Learning Electron
 
--- coverLetter.js
+++ coverLetter.js
@@ -1,18 +1,18 @@
 function generateCL() {
 
     function coverLetter(companyName, position, familiar) {
-        ftext = "Ryan Karumanchery " + "<br />" + " 69 Columbia Street West " + "<br />" + " Waterloo, ON, N2L 3K7 " + "<br />" +
+        ftext = "YOUR NAME HERE " + "<br />" + " ADDRESS LINE 1 " + "<br />" + " ADDRESS LINE 2 " + "<br />" +
             " " + "<br />" + companyName + "<br />" + " To whom it may concern," + "<br />" +
             "Thank you for the opportunity to apply to the " + position +
             " role at your company. After reviewing your job description, it’s clear that you’re looking for a candidate that is extremely familiar with " +
             familiar +
             ". Given these requirements, I am certain that I have the necessary skills to successfully do the job adeptly and perform above expectations. " +
             "<br />" + "<br />" +
-            " I am a hard-working university student (Bachelor of Mathematics) currently attending the University of Waterloo. During my academic career, I also managed to further develop my professional skills by completing courses where I learned many valuable skills including algorithm design, data abstraction, authentication, and web development. In both my academic and professional life, I have been consistently praised as quality-oriented by my professors and peers. Whether working on academic, extracurricular, or professional projects, I apply proven problem-solving and creative thinking skills, which I hope to leverage into the " +
+            " I am a hard-working student currently attending YOUR SCHOOL. During my academic career, I also managed to further develop my professional skills by INSERT STUFF HERE..., which I hope to leverage into the " +
             position + " role at " + companyName + ". " + "<br />" + "<br />" +
-            " After reviewing my resume, I hope you will agree that I am the type of competent and competitive candidate that you are looking for. I look forward to elaborating on how my specific skills and abilities will benefit your organization. Please contact me at (647) 505-2237 or via email at ryankarumanchery@gmail.com to arrange a convenient meeting time. " +
+            " After reviewing my resume, I hope you will agree that I am the greatest. I look forward to elaborating on how my greatness will benefit your organization. Please contact me at my PHONE NUMBER or via email at EMAIL HERE to arrange a convenient meeting time. " +
             "<br />" + "<br />" + "Thank you for your consideration, and I look forward to hearing from you soon." + "<br />" +
-            "Sincerely," + "<br />" + "Ryan Karumanchery";
+            "Sincerely," + "<br />" + "YOUR NAME";
         return ftext;
     };
 
